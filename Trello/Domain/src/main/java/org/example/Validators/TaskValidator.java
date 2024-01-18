e77535f7c378ae06ff7dcc889245653cd42f67b4
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2024-01-18 20:21:45 +0200

    Add TaskValidator.java

---
+++ Trello/Domain/src/main/java/org/example/Validators/TaskValidator.java
@@ -0,0 +1,11 @@
+package org.example.Validators;
+
+public class TaskValidator {
+
+    public void taskValidateName(String name) {
+
+    }
+    public void taskValidateDescription(String description) {
+
+    }
+}
