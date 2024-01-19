f465952571a02b6f069c6af082ca7073ddddfb27
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2024-01-19 08:05:33 +0200

    Added exception handling

--- Trello/Online_app/src/main/java/org/example/BoardController.java
+++ Trello/Online_app/src/main/java/org/example/BoardController.java
@@ -4,10 +4,12 @@ import org.example.Board.Board;
 import org.example.Board.BoardRepository;
 import org.example.Board.BoardService;
 import org.example.Column.Column;
+import org.example.Exception.BoardInvalidNameException;
 import org.example.Exception.BoardNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,41 +28,45 @@ public class BoardController {
     }
     @GetMapping("/boards")
     public ModelAndView getCreateBoardPage() {
-        try {
-            return new ModelAndView("redirect:/boards/" + boardRepository.getRandomBoardId());
-        } catch (BoardNotFoundException e) {
-            return new ModelAndView("CreateBoard");
-        }
+        return new ModelAndView("CreateBoard");
     }
     @GetMapping("/boards/{boardId}")
     public ModelAndView getTasksAndColumnsForBoard(@PathVariable int boardId) {
-        ModelAndView modelAndView = new ModelAndView("BoardMenu");
-
         List<Board> boards = boardRepository.getAllBoardIds();
+        if (CollectionUtils.isEmpty(boards)) {
+            return new ModelAndView("redirect:/boards");
+        }
         List<List<Column>> columns = boardRepository.getTasksAndColumnsForBoard(boardId);
-
+        ModelAndView modelAndView = new ModelAndView("BoardMenu");
         modelAndView.addObject("columns", columns);
         modelAndView.addObject("boards", boards);
         return modelAndView;
     }
     @PostMapping("/create")
-    public ResponseEntity<String> createBoard(@RequestBody String boardName) {
+    public ResponseEntity<String> createBoard(@RequestBody Map<String, String> data) {
         try {
+            String boardName = data.get("boardName");
             boardService.createBoard(boardName);
-            return ResponseEntity.ok("Board created successfully");
+            return ResponseEntity.ok(boardRepository.getRandomBoardId().toString());
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create board");
         }
     }
     @PutMapping("/updateNameBoard/{boardId}")
-    public ResponseEntity<String> updateBoardName(@PathVariable Long boardId, @RequestBody Map<String, String> data) {
-        boardService.reNameBoard(boardId,  data.get("newName"));
-        return ResponseEntity.ok("Board name updated successfully");
+    public ResponseEntity<String> updateBoardName(@PathVariable Long boardId,
+                                                  @RequestBody Map<String, String> data) {
+        try {
+            boardService.reNameBoard(boardId,  data.get("newName"));
+            return ResponseEntity.ok("Board name updated successfully");
+        } catch (BoardInvalidNameException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to rename board");
+        }
+
     }
     @DeleteMapping("/delete/{boardId}")
-    public ResponseEntity<String> deleteBoard(@PathVariable Long boardId) {
+    public ModelAndView deleteBoard(@PathVariable Long boardId) {
         boardRepository.deleteBoard(boardId);
-        return ResponseEntity.ok("Column deleted successfully");
+        return new ModelAndView("redirect:/boards");
     }
 }
