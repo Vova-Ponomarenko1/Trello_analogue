1e126350740caf9c8184bc1711863ef9fc581fe5
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2024-01-19 07:51:44 +0200

    Initial commit Business logic

--- Trello/Domain/src/main/java/org/example/Validators/TaskValidator.java
+++ Trello/Domain/src/main/java/org/example/Validators/TaskValidator.java
@@ -1,11 +1,20 @@
 package org.example.Validators;
 
+import org.example.Exception.TaskValidateException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TaskValidator {
 
     public void taskValidateName(String name) {
-
+        if (name == null || name.trim().isEmpty() || name.length() > 45) {
+            throw new TaskValidateException("Task name is invalid. It should not be null, empty, or exceed 45 characters.");
+        }
     }
     public void taskValidateDescription(String description) {
-
+        if (description == null || description.length() > 1000) {
+            throw new TaskValidateException("Task description is invalid. It should not be null and should " +
+                    "not exceed 1000 characters.");
+        }
     }
 }
