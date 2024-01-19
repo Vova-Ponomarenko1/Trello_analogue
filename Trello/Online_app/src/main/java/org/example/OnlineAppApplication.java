1d046cf2f60cca8835ec58c0b0f6defacfa11542
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2024-01-19 09:54:27 +0200

    Added information about the starter url

--- Trello/Online_app/src/main/java/org/example/OnlineAppApplication.java
+++ Trello/Online_app/src/main/java/org/example/OnlineAppApplication.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Start url:
+ *  http://localhost:8080/boards
+ */
 @SpringBootApplication
 public class OnlineAppApplication {
 
