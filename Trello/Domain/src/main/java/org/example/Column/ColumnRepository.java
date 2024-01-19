f34493c0ab644bf35db69236b9a003a22f356868
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2024-01-19 08:03:25 +0200

    Added an exception

--- Trello/Domain/src/main/java/org/example/Column/ColumnRepository.java
+++ Trello/Domain/src/main/java/org/example/Column/ColumnRepository.java
@@ -1,5 +1,6 @@
 package org.example.Column;
 
+import org.example.Exception.BoardNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 @Repository
 public interface ColumnRepository {
 
-    void createColumn(Long boardId, String columnName);
+    void createColumn(Long boardId, String columnName) throws BoardNotFoundException;
 
     void updateColumnName(int columnId, String updateName);
 
